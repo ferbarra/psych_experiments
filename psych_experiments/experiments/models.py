db7bbb65062ec3c56d4743fb48bab060fba6a88f
Fernando Barra <barrarod@ualberta.ca>
2022-12-27 23:16:06 -0700

    Experiment model provides counter balanced conditions

--- psych_experiments/experiments/models.py
+++ psych_experiments/experiments/models.py
@@ -7,19 +7,31 @@ class Experiment(models.Model):
     Represents an experiment hosted in Pavlovia.com. Each experiment has a set of conditions. Participants accessing
     the experiment must be assigned a condition in such a way that
     """
-    name = models.CharField()
+    name = models.CharField(max_length=200)
+
+    # 0 based index used for counter balancing experiment conditions
     last_condition_index = models.IntegerField(default=0, validators=[MinValueValidator(0)])
+
     pavlovia_url = models.URLField()
 
-    def next_condition(self):
-        pass
+    def get_counter_balanced_condition(self):
+        conditions = self.condition_set.all()
+        condition = conditions[self.last_condition_index]
+
+        # update the index round-robin like
+        if self.last_condition_index == len(conditions) - 1:
+            self.last_condition_index = 0
+        else:
+            self.last_condition_index += 1
+
+        return condition
 
     def __str__(self):
         return self.name
 
 
 class Condition(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=200)
     experiment = models.ForeignKey(Experiment, on_delete=models.CASCADE)
 
     class Meta:
--- psych_experiments/experiments/tests.py
+++ psych_experiments/experiments/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from psych_experiments.experiments.models import Experiment, Condition
 
-# Create your tests here.
+
+class ExperimentTestCase(TestCase):
+
+    def setUp(self):
+        experiment = Experiment(name="Test Experiment", pavlovia_url="www.pavlovia.org")
+        experiment.save()
+        # create some conditions for the experiment
+        experiment.condition_set.create(name='A')
+        experiment.condition_set.create(name='X')
+        experiment.condition_set.create(name='P')
+        experiment.condition_set.create(name='H')
+
+    def test_next_condition(self):
+        experiment = Experiment.objects.filter(name="Test Experiment")[0]
+        conditions = experiment.condition_set.all()
+        count_by_conditions = {}
+        for x in range(0, len(conditions) * 2):
+            counter_balanced_condition = experiment.get_counter_balanced_condition()
+            if count_by_conditions.get(counter_balanced_condition.name, None) is None:
+                count_by_conditions[counter_balanced_condition.name] = 0
+            count_by_conditions[counter_balanced_condition.name] +=1
+
+        # Verify that all the conditions have been returned the same number of times
+        self.assertEqual(2, count_by_conditions['A'])
+        self.assertEqual(2, count_by_conditions['X'])
+        self.assertEqual(2, count_by_conditions['P'])
+        self.assertEqual(2, count_by_conditions['H'])
